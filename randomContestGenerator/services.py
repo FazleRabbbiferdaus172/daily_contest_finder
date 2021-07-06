da2c0636bce085090e36f7332a7ca98e80386cab
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-07-06 08:03:42 +0600

    state_management_done

--- randomContestGenerator/services.py
+++ randomContestGenerator/services.py
@@ -4,6 +4,7 @@ import random
 
 
 class GrabingJSON:
+
     def __init__(self):
         self.resposeJSON = requests.get(
             'https://codeforces.com/api/contest.list?gym=false').json()
@@ -16,5 +17,16 @@ class GrabingJSON:
                 all.append((i['id'], i['name']))
         temp = random.choice(all)
         contest = {
-            'url': "https://codeforces.com/contestRegistration/1542/virtual/true".format(temp[0]), 'name': temp[1]}
+            'url': "https://codeforces.com/contestRegistration/{}/virtual/true".format(temp[0]), 'name': temp[1], 'id': temp[0]}
         return contest
+
+    def has_participated(self, handle='fazle_rabbi_ferdaus', contest_id=''):
+        if not contest_id:
+            return True
+        api_url = 'https://codeforces.com/api/contest.status?contestId={}&handle={}&from=1&count=1000'.format(
+            contest_id, handle)
+        participated_resposeJSON = requests.get(api_url).json()
+        if participated_resposeJSON['result']:
+            return True
+        else:
+            return False
--- randomContestGenerator/views.py
+++ randomContestGenerator/views.py
@@ -3,6 +3,9 @@ from django.views.generic import TemplateView
 from . import services
 # Create your views here.
 
+state = {'id': '', 'url': '#',
+         'name': 'Generate Todays Contest', 'status': False}
+
 
 class Index(TemplateView):
     template_name = 'index.html'
@@ -12,14 +15,29 @@ class RandomContest(TemplateView):
     template_name = 'randomContest.html'
 
     def get_context_data(self, **kwargs):
+        global state
         context = super().get_context_data(**kwargs)
         context['contest'] = {
-            'url': "#", 'name': "Generate Todays Contest"}
+            'url': state['url'], 'name': state['name'], 'id': state['id']}
         return context
 
 
 def generate_random_contest(request):
+    global state
     if request.method == "GET":
         contest = services.GrabingJSON()
-        context = {'contest': contest.grabContest('3')}
+
+        if state['status'] or contest.has_participated(handle='fazle_rabbi_ferdaus', contest_id=state['id']):
+            context = {'contest': contest.grabContest('3')}
+            state['id'] = context['contest']['id']
+            state['url'] = context['contest']['url']
+            state['name'] = context['contest']['name']
+            state['status'] = contest.has_participated(
+                handle='fazle_rabbi_ferdaus', contest_id=state['id'])
+            print(state)
+
+        else:
+            context = {'contest': {
+                'url': state['url'], 'name': state['name'], 'id': state['id']}}
+
         return render(request, "randomContest.html", context=context)
