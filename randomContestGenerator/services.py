88321ac7df3db7fb52f3b5d92147067dba64f4de
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-07-11 04:08:07 +0600

    new service

--- randomContestGenerator/services.py
+++ randomContestGenerator/services.py
@@ -16,8 +16,12 @@ class GrabingJSON:
             if contest_type in i['name'] and i['phase'] == "FINISHED":
                 all.append((i['id'], i['name']))
         temp = random.choice(all)
+        # contest = {
+        #     'url': "https://codeforces.com/contestRegistration/{}/virtual/true".format(temp[0]), 'name': temp[1], 'id': temp[0]}
+
         contest = {
-            'url': "https://codeforces.com/contestRegistration/{}/virtual/true".format(temp[0]), 'name': temp[1], 'id': temp[0]}
+            'url': "https://codeforces.com/contest/{}".format(temp[0]), 'name': temp[1], 'id': temp[0]}
+
         return contest
 
     def grabAllcontest(self, t='3'):
@@ -28,6 +32,13 @@ class GrabingJSON:
                 all.append(
                     {'url': 'https://codeforces.com/contest/{}'.format(i['id']), 'name': i['name']})
         # print(all)
+        return all
+
+    def grabCustomeProblemSet(self, number, dif_choice, tags):
+        api_url = 'https://codeforces.com/api/problemset.problems'
+        all = []
+        problem_set_responseJSON = participated_resposeJSON = requests.get(
+            api_url).json()
         return all
 
     def has_participated(self, handle='', contest_id=''):
